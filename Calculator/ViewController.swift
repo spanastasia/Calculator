6e4f2b35170b22c0c239626c530e597f477ac818
ChTsTPYv <chtstpyv@email.com>
2020-09-11 09:22:37 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Anastasiia Spiridonova on 11.09.2020.
+//  Copyright © 2020 Anastasiia Spiridonova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
